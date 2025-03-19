86ddde62747cfbf1f45b55ab6434af9f538f2036
Luis Pacheco <laps1308@gmail.com>
2025-03-19 17:14:16 -0500

    ft("components.json"): I added shadcn component into project

--- AgendateComponents/Headers/GeneralHeader.tsx
+++ AgendateComponents/Headers/GeneralHeader.tsx
@@ -3,11 +3,23 @@ import icon from "@/assets/images/agendate_logo.png";
 import { ThemeSelector } from '../ThemeSelector';
 export function GeneralHeader(){
     return <nav className='w-full h-16 md:h-10 lg:h-10 xl:h-10 2xl:h-10 flex flex-column md:flex-row lg:flex-row xl:flex-row justify-center items-center'>
-        <div className='w-full md:w-1/3 lg:w-1/3 xl:w-1/3 2xl:w-1/3 flex flex-row justify-start items-start px-2.5'>
+        <div className='w-full md:w-[40%] lg:-[40%] xl:w-[40%] 2xl:w-[40%] flex flex-row justify-start items-start px-2.5'>
             <Image src={icon.src} alt='Agendate Icon' title='Agendate' width={40} height={40}/>
         </div>
-        <div className='w-full md:w-1/3  lg:w-1/3 xl:w-1/3 2xl:w-1/3 flex flex-row justify-center items-center'></div>
-        <div className='w-full md:w-1/3  lg:w-1/3 xl:w-1/3 2xl:w-1/3 flex flex-row justify-end items-center px-6'>
+        <div className='w-full md:w-[50%]  lg:-[50%] xl:-[50%] 2xl:-[50%] flex flex-row justify-center items-center'>
+            <ul className='w-full flex flex-row justify-center items-center md:justify-end lg:justify-end xl:justify-end 2xl:justify-end gap-[10px]'>
+                <li>
+                    <a>Inicio</a>
+                </li>
+                <li>
+                    <a>Ingresar</a>
+                </li>
+                <li>
+                    <a>Registrate</a>
+                </li>
+            </ul>
+        </div>
+        <div className='w-full md:w-[10%]  lg:w-[10%] xl:w-[10%] 2xl:w-[10%] flex flex-row justify-end items-center px-6'>
             <ThemeSelector/>
         </div>
     </nav>
